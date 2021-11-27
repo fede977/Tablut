52a9aa49b4a76f13b84c2bdf1cfea0d00d1ceb01
Flebio <94052716+Flebio@users.noreply.github.com>
2021-11-27 15:31:17 +0100

    Add files via upload

---
+++ LMstart.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.lang.ProcessBuilder;
+
+public class lastminute{
+
+    public static void main(String[] args) {
+
+        
+        if (args.length < 2) {
+            System.err.println("Usage: java lastminute pawn_color host");
+            System.exit(1);
+        } 
+
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("ant lastminute -Dcolor=");
+        stringBuilder.append(args[0]);
+        stringBuilder.append(" -Dtime=1 -Dserver=");
+        stringBuilder.append(args[1]);
+        
+        String arguments = stringBuilder.toString();
+
+        ProcessBuilder processBuilder = new ProcessBuilder();
+		processBuilder.command("sh", "-c", arguments);
+        Process tablut_game = processBuilder.start(); 
+		System.out.print("The game is aboiut to start...");
+
+    
+
+
+    }
+}
